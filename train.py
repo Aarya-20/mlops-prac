4ef4aba7ba594daf3d7e7df1893a9e5e73609ae5
Aarya-20 <aryajagtap404@gmail.com>
2026-04-22 17:06:37 +0530

    Renamed file to train.py and updated model to v2

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = DecisionTreeClassifier(max_depth=3)
+model = DecisionTreeClassifier(max_depth=3,criterion="entropy",random_state=42)
 model.fit(X_train, y_train)
 
 print("Accuracy:", model.score(X_test, y_test))
@@ -20,3 +20,4 @@ with open("model_v1.pkl", "wb") as f:
     pickle.dump(model, f)
 
 print("Model saved")
+print("this is version 2")
