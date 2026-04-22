0f818b3ba14589b472e52e80090aaabf633e562d
Aarya-20 <aryajagtap404@gmail.com>
2026-04-22 16:32:46 +0530

    comment

---
+++ from sklearn.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = DecisionTreeClassifier(max_depth=3)
+model.fit(X_train, y_train)
+
+print("Accuracy:", model.score(X_test, y_test))
+
+with open("model_v1.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model saved")
